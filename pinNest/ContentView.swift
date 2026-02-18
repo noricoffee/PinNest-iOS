77efbd6d94659ed8c15981e0ab9061c265957888
noricoffee <yoshidanoriyuki@yoshidanoriyukinoMacBook-Pro.local>
2026-02-18 23:16:46 +0900

    initial commit

---
+++ pinNest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pinNest
+//
+//  Created by 吉田範之 on 2026/02/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pinNest/pinNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinNestApp.swift
+//  pinNest
+//
+//  Created by 吉田範之 on 2026/02/18.
+//
+
+import SwiftUI
+
+@main
+struct pinNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
